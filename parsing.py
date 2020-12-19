a79e2047acd83182676952f19fe8f671a68eaa50
Somik92 <samsonov.work@gmail.com>
2020-12-19 17:28:47 +0300

    rename script

--- parsing.py
+++ parsing.py
@@ -23,19 +23,23 @@ def get_data(ticker, country, column):
     df['Open'] = df['Open'].astype(float, round(2))
     df['change'] = df['Close'][-1] - df['Close'][-2]
     df['change'] = round(df['change'], 3)
-
     df['percent'] = df['change'] / df['Close'][-1] * 100
     df['percent'] = round(df['percent'], 3)
-
-
+    if column != 'Close':
+        if df.last('1D')[column].item() >= 0:
+            return '+' + str(list(df.last('1D')[column])).replace('[', '').replace(']', '')
+        elif df.last('1D')[column].item() < 0:
+            return '-' + str(list(df.last('1D')[column])).replace('[', '').replace(']', '')
     return str(list(df.last('1D')[column])).replace('[', '').replace(']', '')
 
+
+
 def line(dlina, shirina, colour):
-    green_line = Image.open('/Users/somik/Desktop/Telegram/'+colour+'_line.png').convert("RGBA")
+    green_line = Image.open('/Users/somik/Desktop/Telegram/'+ colour+'_line.png').convert("RGBA")
     im.paste(green_line, (dlina, shirina), green_line)
-def usa(dlina, shirina):
-    usa = Image.open('/Users/somik/Desktop/Telegram/usa120.png').convert("RGBA")
-    im.paste(usa, (dlina, shirina), usa)
+def logo(dlina, shirina, name):
+    logo = Image.open('/Users/somik/Desktop/Telegram/' + name + '.png').convert("RGBA")
+    im.paste(logo, (dlina, shirina), logo)
 def text(text, dlina, shirina, colour, font):
     draw_text.text(
         (dlina, shirina),
@@ -47,12 +51,14 @@ def text(text, dlina, shirina, colour, font):
 
 
 line(50, 100, 'green')
-line(50, 200, 'red')
-line(50, 300, 'purple')
+line(50, 200, 'green')
+line(50, 300, 'green')
 
-usa(350, 83)
-usa(350, 183)
-usa(350, 283)
+
+logo(350, 83, 'usa120')
+logo(350, 183, 'usa120')
+logo(350, 283, 'usa120')
+
 
 text('S&P500', 83, 115,'#000000', font_stock)
 text('Dow Jones', 83, 215, '#000000', font_stock)
@@ -61,18 +67,20 @@ text(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 50, 30, '#FFFFFF', f
 
 
 
-text(get_data('S&P 500', 'United States', 'Close'), 483, 120, '#000000', font_quotes)
-text(get_data('Dow 30', 'United States', 'Close'), 483, 220, '#000000', font_quotes)
-text(get_data('Nasdaq', 'United States', 'Close'), 483, 320, '#000000', font_quotes)
+text(get_data('S&P 500', 'United States', 'Close'),
+     483, 120, '#000000', font_quotes)
+text(get_data('Dow 30', 'United States', 'Close'),
+     483, 220, '#000000', font_quotes)
+text(get_data('Nasdaq', 'United States', 'Close'),
+     483, 320, '#000000', font_quotes)
 
-text(get_data('S&P 500', 'United States', 'change'), 710, 120, '#000000', font_quotes)
-text(get_data('Dow 30', 'United States', 'change'), 710, 220, '#000000', font_quotes)
-text(get_data('Nasdaq', 'United States', 'change'), 710, 320, '#000000', font_quotes)
+text(get_data('S&P 500', 'United States', 'change'), 700, 120, '#000000', font_quotes)
+text(get_data('Dow 30', 'United States', 'change'), 700, 220, '#000000', font_quotes)
+text(get_data('Nasdaq', 'United States', 'change'), 700, 320, '#000000', font_quotes)
 
-
-text(get_data('S&P 500', 'United States', 'percent')+ '%', 900, 120, '#000000', font_quotes)
-text(get_data('Dow 30', 'United States', 'percent') + '%', 900, 220, '#000000', font_quotes)
-text(get_data('Nasdaq', 'United States', 'percent') + '%', 900, 320, '#000000', font_quotes)
+text(get_data('S&P 500', 'United States', 'percent')+ '%', 880, 120, '#000000', font_quotes)
+text(get_data('Dow 30', 'United States', 'percent') + '%', 880, 220, '#000000', font_quotes)
+text(get_data('Nasdaq', 'United States', 'percent') + '%', 880, 320, '#000000', font_quotes)
 
 
 im.show()
